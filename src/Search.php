fd42a7be06d12975349bcc0200907dc788358e47
nam <tiennam1999hp@gmail.com>
2019-11-21 14:14:36 +0700

    BoldWord

---
+++ src/Search.php
@@ -0,0 +1,16 @@
+<?php
+
+final class Search
+{
+    public function searchWord($paragraph, $word) {
+        $paragraph = preg_split("/\s/", $paragraph);
+        $word = preg_split("/\s/", $word);
+        $equal1 = array_intersect($paragraph, $word);
+        $equal2 = array_intersect($paragraph, $word);
+        foreach($equal1 as &$temp) {
+            $temp = "<strong>$temp</strong>";
+        }
+
+        return implode(" ", str_replace($equal2, $equal1, $paragraph));
+    }
+}
---
+++ tests/SearchTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class SearchTest extends TestCase
+{
+    public function testSearch() {
+        $this->assertEquals(
+            '<strong>day</strong> la <strong>doan</strong> van',
+            Search::searchWord('day la doan van','day doan')
+        );
+    }
+}
